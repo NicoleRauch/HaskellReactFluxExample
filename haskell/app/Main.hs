fdf28aa38b4f9941cc76120c05d87d31a3b44bac
Nicole Rauch <nicole.m@gmx.de>
2017-02-22 11:22:12 +0100

    Another demonstrator app for a react-flux issue.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import qualified React.Flux  as F
-import ReactFluxExample
+import ReactFluxExample_Lifecycle
 
 -- the first argument of reactRender is the id of the DOM element in index.html that the app will be rendered into
 main :: IO ()
---
+++ haskell/src/ReactFluxExample_Lifecycle.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module ReactFluxExample_Lifecycle where
+
+import           Control.DeepSeq
+import           Data.Monoid ((<>))
+import           Data.Typeable (Typeable)
+import           GHC.Generics (Generic)
+import           React.Flux
+import           React.Flux.Lifecycle
+
+{- NOINLINE exampleApp  -}
+{- NOINLINE lifecycleC  -}
+{- NOINLINE exampleStore  -}
+
+exampleApp :: ReactView ()
+exampleApp = defineControllerView "ExampleApp" exampleStore $ \(ExampleState visible) () ->
+  div_ $ do
+    button_ [ onClick $ \_ _ -> dispatch ToggleVisible ] "Click here!"
+    div_ . elemString $ "Outside: " ++ show visible
+    lifecycleC_ $ do
+        div_ . elemString $ "Inside: " ++ show visible
+
+
+lifecycleC :: ReactView ()
+lifecycleC = defineLifecycleView "ALifecycleComponent" () lifecycleConfig
+   { lRender = \_ _ -> childrenPassedToView
+   }
+
+lifecycleC_ :: ReactElementM eventHandler () -> ReactElementM eventHandler ()
+lifecycleC_ = view lifecycleC ()
+
+data ExampleState = ExampleState
+  { isVisible :: Bool
+  } deriving (Show, Typeable)
+
+data ExampleAction = ToggleVisible
+  deriving (Show, Typeable, Generic, NFData)
+
+instance StoreData ExampleState where
+    type StoreAction ExampleState = ExampleAction
+    transform action state = do
+        putStrLn $ "Old state: " <> show state
+        putStrLn $ "Action: " <> show action
+
+        let newState = case action of
+                     ToggleVisible -> state { isVisible = not $ isVisible state }
+
+        putStrLn $ "New state: " <> show newState
+        return newState
+
+exampleStore :: ReactStore ExampleState
+exampleStore = mkStore $ ExampleState False
+
+dispatch :: ExampleAction -> [SomeStoreAction]
+dispatch a = [SomeStoreAction exampleStore a]
